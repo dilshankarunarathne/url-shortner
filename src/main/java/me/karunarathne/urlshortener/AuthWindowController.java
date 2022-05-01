fd66c1f01a7da748b13db67ab0b724fb1a036345
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-05-01 11:38:52 +0530

    Update AuthWindowController.java

--- src/main/java/me/karunarathne/urlshortener/AuthWindowController.java
+++ src/main/java/me/karunarathne/urlshortener/AuthWindowController.java
@@ -1,4 +1,26 @@
-package me.karunarathne.urlshortener;
+package me.karunarathne.urlshortener ;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class AuthWindowController {
+    @FXML
+    private Label statusLabel ;
+
+    @FXML
+    private TextField userId ;
+    @FXML
+    private TextField puk ;
+    @FXML
+    private TextField sk ;
+
+    public AuthWindowController () {
+    }
+
+    @FXML
+    public void authenticate(ActionEvent actionEvent) {
+        System.out.println("Auth called with " + userId.getText());
+    }
 }
