1d88ba8698c873a659cb3e87d8cb90f1fa4022b2
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-05-01 14:38:32 +0530

    Update AuthWindow.java

--- src/main/java/me/karunarathne/urlshortener/AuthWindow.java
+++ src/main/java/me/karunarathne/urlshortener/AuthWindow.java
@@ -3,6 +3,7 @@ package me.karunarathne.urlshortener;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,7 +15,7 @@ public class AuthWindow extends Application {
         Scene scene = new Scene(fxmlLoader.load(), 720, 480);
         stage.setTitle("URL Shortener");
         stage.setScene(scene);
-        Image icon = new Image(getClass().getResourceAsStream("icon.png"));
+        Image icon = new Image( getClass().getResourceAsStream ("icon.png"));
         stage.getIcons().add(icon);
         stage.show();
     }
